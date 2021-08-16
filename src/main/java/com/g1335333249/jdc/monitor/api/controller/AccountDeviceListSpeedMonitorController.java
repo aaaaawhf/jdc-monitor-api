ca31a56f1e01c7e98a9edbb13aba9edc4132908e
guanpeng <guanpeng@huimei.com>
2021-08-16 17:02:51 +0800

    init

--- src/main/java/com/g1335333249/jdc/monitor/api/controller/AccountDeviceListSpeedMonitorController.java
+++ src/main/java/com/g1335333249/jdc/monitor/api/controller/AccountDeviceListSpeedMonitorController.java
@@ -1,13 +1,25 @@
 package com.g1335333249.jdc.monitor.api.controller;
 
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.g1335333249.jdc.monitor.api.entity.AccountDeviceListSpeedMonitor;
+import com.g1335333249.jdc.monitor.api.model.request.AccountDeviceListSpeedMonitorRequestParam;
+import com.g1335333249.jdc.monitor.api.service.IAccountDeviceListSpeedMonitorService;
+import com.g1335333249.jdc.monitor.api.utils.Result;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 /**
  * <p>
- *  前端控制器
+ * 前端控制器
  * </p>
  *
  * @author guanpeng
@@ -17,4 +29,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/account-device-list-speed-monitor")
 public class AccountDeviceListSpeedMonitorController {
 
+    @Autowired
+    private IAccountDeviceListSpeedMonitorService iAccountDeviceListSpeedMonitorService;
+
+    @PostMapping("search")
+    public Result<List<AccountDeviceListSpeedMonitor>> search(@RequestBody AccountDeviceListSpeedMonitorRequestParam request) {
+        Calendar instance = Calendar.getInstance();
+        instance.set(Calendar.HOUR, 0);
+        instance.set(Calendar.MINUTE, 0);
+        instance.set(Calendar.SECOND, 0);
+        LambdaQueryWrapper<AccountDeviceListSpeedMonitor> query = Wrappers.lambdaQuery();
+        query.eq(AccountDeviceListSpeedMonitor::getAccountDeviceListId, request.getAccountDeviceListId());
+        query.between(AccountDeviceListSpeedMonitor::getCreateTime, instance, new Date());
+        query.orderByAsc(AccountDeviceListSpeedMonitor::getCreateTime);
+        return Result.success(iAccountDeviceListSpeedMonitorService.list(query));
+    }
 }
---
+++ src/main/java/com/g1335333249/jdc/monitor/api/model/request/AccountDeviceListSpeedMonitorRequestParam.java
@@ -0,0 +1,14 @@
+package com.g1335333249.jdc.monitor.api.model.request;
+
+import lombok.Data;
+
+/**
+ * @Author: guanpeng
+ * @Date: Create at 2021/8/16 16:56
+ * @Description:
+ * @Modified By:
+ */
+@Data
+public class AccountDeviceListSpeedMonitorRequestParam {
+    private Long accountDeviceListId;
+}
