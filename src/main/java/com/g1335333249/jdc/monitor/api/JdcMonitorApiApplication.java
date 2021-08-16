982af7d4de6e8cae38d46fba914705ba1544acd7
guanpeng <guanpeng@huimei.com>
2021-08-16 16:41:41 +0800

    init

--- src/main/java/com/g1335333249/jdc/monitor/api/JdcMonitorApiApplication.java
+++ src/main/java/com/g1335333249/jdc/monitor/api/JdcMonitorApiApplication.java
@@ -4,11 +4,13 @@ import com.g1335333249.jdc.monitor.api.security.config.SpringContextHolder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author guanpeng
  */
 @SpringBootApplication
+@EnableScheduling
 public class JdcMonitorApiApplication {
 
     public static void main(String[] args) {
